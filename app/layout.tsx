bd0926a46aa752c439abaab68aa71f32e24bc42b
Arif <arif.zinlab@gmail.com>
2026-01-09 11:27:52 +0500

    Add Google AdSense script to layout for ad integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="scroll-smooth">
+            <head>
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9109061590646279"
+                    crossOrigin="anonymous"
+                />
+            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
